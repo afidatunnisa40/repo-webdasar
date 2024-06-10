eabc37c2c1a8f2f3fe981281ae6bce2926839aa3
afidatunnisa40 <142140352+afidatunnisa40@users.noreply.github.com>
2024-06-10 17:05:33 +0700

    Add files via upload
    first commit

---
+++ WebDasar/assets/js/script.js
@@ -0,0 +1,6 @@
+function onButtonClick() {
+    alert('Your suggestion has been sent !');
+}
+  
+  const button = document.querySelector('button');
+  button.addEventListener('click', onButtonClick);
